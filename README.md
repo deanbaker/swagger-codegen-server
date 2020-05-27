5b700cd9b725e45f141f363b863299eaf39356b4
Dean Baker <dean.baker@pexa.com.au>
2020-05-27 20:36:45 +1000

    Add petstore generated spec

---
+++ README.md
@@ -0,0 +1,9 @@
+# Swagger server generator
+
+Example codebase to show how we can design API first with OAS generating a bunch of generated code.
+
+## Curl test
+
+
+`curl --location --request GET 'localhost:8080/api/pet/1' --header 'Accept: application/json' | json_pp`
+
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +19,20 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+/**
+ * Component to list out all of our endpoints at startup.
+ */
+@Component
+class EndpointsListener {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EndpointsListener.class);
+
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) {
+		ApplicationContext applicationContext = event.getApplicationContext();
+		applicationContext.getBean(RequestMappingHandlerMapping.class)
+				.getHandlerMethods().forEach((key, value) -> LOGGER.info("{} {}", key, value));
+	}
 }
