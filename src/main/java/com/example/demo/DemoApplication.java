5632cffa56c1cf7c66d47921daf31e64e31666f8
Dean Baker <dean.baker@pexa.com.au>
2020-05-28 10:43:00 +1000

    Add delegate pattern

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.api.PetApiDelegate;
+import com.example.demo.api.model.Pet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class DemoApplication {
